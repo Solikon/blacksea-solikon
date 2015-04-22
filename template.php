2ed56ffa06d1cd09154e0c13285e7d1679261195
rainer <rainer@rhalbmann.de>
2015-04-22 12:20:27 +0200

    scripts-style1.js aus template.php mit scripts.js ersetzt, theme von contrib nach custom verschoben

--- template.php
+++ template.php
@@ -10,19 +10,19 @@ $style = theme_get_setting('style');
 
 switch ($style) {
 	case 0:
-		drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts-style1.js"); 
+		drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts-style1.js");
 		drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/style1.css', array('weight' => CSS_THEME, 'type' => 'file'));
 		break;
 	default:
   	drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts-style1.js");
 		drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/style1.css', array('weight' => CSS_THEME, 'type' => 'file'));
-		
+
 			case 1:
-		drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts-style1.js"); 
+		drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts.js");
 		drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/style-solikon.css', array('weight' => CSS_THEME, 'type' => 'file'));
 		break;
 	default:
-  	drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts-style1.js");
+  	drupal_add_js(drupal_get_path('theme', 'bs_solikon') . "/js/scripts.js");
 		drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/style-solikon.css', array('weight' => CSS_THEME, 'type' => 'file'));
 }
 
@@ -31,5 +31,5 @@ switch ($style) {
 $css = theme_get_setting('css');
 
   if  ($css == 1) {
-    drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/custom.css', array('weight' => CSS_THEME, 'type' => 'file')); 
-  } 
+    drupal_add_css(drupal_get_path('theme', 'bs_solikon') . '/css/custom.css', array('weight' => CSS_THEME, 'type' => 'file'));
+  }
